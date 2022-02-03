895361c93815adb139a21558f5b7fc7022cd632b
hedenica <hedenica@hotmail.com>
2022-02-03 16:44:59 -0300

    refact: remove unused dependencies

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    urlImports: [
-        "https://framer.com/m/",
-        "https://framerusercontent.com/",
-        "https://ga.jspm.io/",
-        "https://jspm.dev/",
-    ],
-},
 }
 
 module.exports = nextConfig
