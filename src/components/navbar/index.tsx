f79aa96c8e7eafdd69ce092bed2313885bd9008c
hedenica <hedenica@hotmail.com>
2022-02-03 16:31:34 -0300

    feat: add navbar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,64 @@
+import { motion } from 'framer-motion';
+
+import styles from './navbar.module.css';
+
+function Navbar() {
+  const logoVariants = {
+    initial: {
+      opacity: 0,
+      x: '-300px',
+    },
+    animate: {
+      opacity: 1,
+      x: '20px',
+      transition: {
+        type: 'spring',
+        stiffness: 100,
+      }
+    },
+  }
+
+  const tagVariants = {
+    initial: {
+      opacity: 0
+    },
+    animate: {
+      opacity: 1,
+      transition: {
+        duration: 2,
+        delay: 1,
+        ease: 'easeInOut',
+      }
+    },
+  }
+
+  return (
+    <nav className={styles.navbar}>
+      <motion.h2 
+        variants={logoVariants} 
+        initial="initial" 
+        animate="animate" 
+        className={styles.logo}
+      >
+        <motion.span
+          variants={tagVariants} 
+          initial="initial" 
+          animate="animate" 
+          className={styles.tag}
+        >{'< '}
+        </motion.span>
+          hedênica
+        <motion.span
+          variants={tagVariants} 
+          initial="initial" 
+          animate="animate"
+          className={styles.tag}
+        >
+        {' />'}
+        </motion.span>
+      </motion.h2>
+    </nav>
+  )
+}
+
+export default Navbar
