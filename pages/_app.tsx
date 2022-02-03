df9ed65a87b38df5ffabb9a75b3520dffc74c65f
hedenica <hedenica@hotmail.com>
2022-02-03 16:38:30 -0300

    setup: general settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Navbar from '../src/components/navbar'
+import Footer from '../src/components/footer'
+
+import '../styles/globals.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  )
 }
 
 export default MyApp
