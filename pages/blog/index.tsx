238b0ddff72addf1b6abee04ca0ffd71efc2a1eb
hedenica <hedenica@hotmail.com>
2022-03-31 11:17:30 -0300

    🐛 fix(typescript): fix typescript warnings by checking if undefined first

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,25 +1,28 @@
-import { usePostsQuery } from "../../src/generated/graphql";
-import { PostCard } from "../../src/components/postCard";
+import { usePostsQuery } from '../../src/generated/graphql'
+import { PostCard } from '../../src/components/postCard'
 
 import styles from './posts.module.css'
 
 export default function Blog() {
-  const { data: { posts } = {} } = usePostsQuery();
+  const { data: { posts } = {} } = usePostsQuery()
 
   return (
     <div className={styles.posts}>
       {posts?.map((post) => (
-        <PostCard key={post.slug} post={{
-          title: post.title,
-          imageUrl: post.coverImage?.url,
-          href: `blog/${post.slug}`,
-          description: post.excerpt,
-          author:{
-            name: post.author?.name,
-            imageUrl:  post.author?.picture?.url,
-          },
-          date: new Date(post.publishedAt).toDateString(),
-        }} />
+        <PostCard
+          key={post.slug}
+          post={{
+            title: post.title,
+            imageUrl: post.coverImage ? post.coverImage.url : '',
+            href: `blog/${post.slug}`,
+            description: post.excerpt,
+            author: {
+              name: post.author ? post.author.name : '',
+              imageUrl: post.author?.picture ? post.author.picture.url : '',
+            },
+            date: new Date(post.publishedAt).toDateString(),
+          }}
+        />
       ))}
     </div>
   )
